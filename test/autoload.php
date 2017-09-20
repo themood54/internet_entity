08c3d9ff4d23b8bf10bf154f28a4de54ea0b30e1
mark moody <elgoogmex@gmail.com>
2017-09-20 01:05:44 -0500

    Delete autoload.php

--- test/autoload.php
+++
@@ -1,5 +0,0 @@
-include_once __DIR__.'/../vendor/autoload.php';
-
-$classLoader = new \Composer\Autoload\ClassLoader();
-$classLoader->addPsr4("Your\\Test\\Namespace\\Here\\", __DIR__, true);
-$classLoader->register();
